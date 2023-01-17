07aca990b7d67846cf56bc4a52661805c340a28c
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2023-01-17 13:26:03 +0300

    task2 fix

--- src/GameSavingLoader.js
+++ src/GameSavingLoader.js
@@ -2,19 +2,15 @@ import read from './reader';
 import json from './parser';
 
 export default class GameSavingLoader {
-  static load() {
-    return new Promise((resolve) => {
-      (async () => {
-        try {
-          const data =  await read();
-          const value = await json(data);
-          console.log(JSON.parse(value))
-          resolve(JSON.parse(value))
-        }
-        catch(err) {
-          console.log(err);
-        }
-      })();
-    });
+  static async load() {
+    try {
+      const data = await read();
+      const value = await json(data);
+      console.log(JSON.parse(value))
+      return JSON.parse(value);
+    }
+    catch(err) {
+      console.log(err);
+    };
   }
 }
